f1dff25c5ccab0257f6d27c8bdb7e9c465cf2244
joaopaulo-afk <85767300+joaopaulo-afk@users.noreply.github.com>
2022-11-12 01:00:24 -0300

    commit by JP

--- src/main/java/com/api/astepi/services/SecretarioService.java
+++ src/main/java/com/api/astepi/services/SecretarioService.java
@@ -1,10 +1,15 @@
 package com.api.astepi.services;
 
+import com.api.astepi.models.AdministradorModel;
 import com.api.astepi.models.SecretarioModel;
 import com.api.astepi.repositories.SecretarioRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @Service
@@ -17,5 +22,15 @@ public class SecretarioService {
     }
 
     @Transactional
-    public SecretarioModel save(SecretarioModel administradorModel) { return secretarioRepository.save(administradorModel); }
+    public SecretarioModel save(SecretarioModel secretarioModel) { return secretarioRepository.save(secretarioModel); }
+
+    public Page<SecretarioModel> findAll(Pageable pageable){ return secretarioRepository.findAll(pageable); }
+
+    public Optional<SecretarioModel> finByID(UUID id) {
+        return secretarioRepository.findById(id);
+    }
+    @Transactional
+    public void delete(SecretarioModel secretarioModel) {
+        secretarioRepository.delete(secretarioModel);
+    }
 }

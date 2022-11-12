364d2449c39f6595079e8f8e111adf2b1c0e6e7a
paulovmns <94497986+paulovmns@users.noreply.github.com>
2022-11-12 02:04:35 -0300

    Commit by Paulo - file update

--- src/main/java/com/api/astepi/services/EnderecoService.java
+++ src/main/java/com/api/astepi/services/EnderecoService.java
@@ -1,12 +1,17 @@
 package com.api.astepi.services;
 
+import com.api.astepi.models.DocumentoModel;
 import com.api.astepi.models.EnderecoModel;
 import com.api.astepi.models.PessoaModel;
 import com.api.astepi.repositories.EnderecoRepository;
 import com.api.astepi.repositories.PessoaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class EnderecoService {
@@ -20,4 +25,18 @@ public class EnderecoService {
     public EnderecoModel save(EnderecoModel enderecoModel) {
         return enderecoRepository.save(enderecoModel);
     }
+
+    public Page<EnderecoModel> findAll(Pageable pageable){
+        return enderecoRepository.findAll(pageable);
+    }
+
+    public Optional<EnderecoModel> finByID(UUID id) {
+        return enderecoRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(EnderecoModel enderecoModel) {
+        enderecoRepository.delete(enderecoModel);
+    }
+
 }

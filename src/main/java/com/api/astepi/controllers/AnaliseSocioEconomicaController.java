5376194f87a3df7aa15d46be19ba4dd9b564078f
paulovmns <94497986+paulovmns@users.noreply.github.com>
2022-11-12 02:02:12 -0300

    Commit by Paulo - file update

--- src/main/java/com/api/astepi/controllers/AnaliseSocioEconomicaController.java
+++ src/main/java/com/api/astepi/controllers/AnaliseSocioEconomicaController.java
@@ -2,12 +2,16 @@ package com.api.astepi.controllers;
 
 
 import com.api.astepi.dtos.AnaliseSocioEconomicaDto;
-import com.api.astepi.dtos.PessoaDto;
+import com.api.astepi.dtos.AnaliseSocioEconomicaDto;
 import com.api.astepi.models.AnaliseSocioEconomicaModel;
-import com.api.astepi.models.PessoaModel;
+import com.api.astepi.models.AnaliseSocioEconomicaModel;
 import com.api.astepi.services.AnaliseSocioEconomicaService;
-import com.api.astepi.services.PessoaService;
+import com.api.astepi.services.AnaliseSocioEconomicaService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +19,12 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/AnaliseSocioEconomicaModel")
+@RequestMapping("/analisesocioeconomicas")
 public class AnaliseSocioEconomicaController {
     final AnaliseSocioEconomicaService analiseSocioEconomicaService;
 
@@ -32,4 +38,41 @@ public class AnaliseSocioEconomicaController {
         BeanUtils.copyProperties(analiseSocioEconomicaDto,analiseSocioEconomicaModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(analiseSocioEconomicaService.save(analiseSocioEconomicaModel));
     }
+
+    @GetMapping
+    public ResponseEntity<Page<AnaliseSocioEconomicaModel>> getAllAnaliseSocioEconomica(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(analiseSocioEconomicaService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneAnaliseSocioEconomica(@PathVariable (value = "id") UUID id){
+        Optional<AnaliseSocioEconomicaModel> analiseSocioEconomicaModelOptional = analiseSocioEconomicaService.finByID(id);
+        if(!analiseSocioEconomicaModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("AnaliseSocioEconomica not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(analiseSocioEconomicaModelOptional.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteAnaliseSocioEconomica(@PathVariable (value = "id")UUID id){
+        Optional<AnaliseSocioEconomicaModel> analiseSocioEconomicaModelOptional = analiseSocioEconomicaService.finByID(id);
+        if(!analiseSocioEconomicaModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("AnaliseSocioEconomica not found.");
+        }
+        analiseSocioEconomicaService.delete(analiseSocioEconomicaModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("AnaliseSocioEconomica deleted successfully.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateAnaliseSocioEconomica(@PathVariable(value = "id")UUID id,@RequestBody @Valid AnaliseSocioEconomicaDto analiseSocioEconomicaDto){
+        Optional<AnaliseSocioEconomicaModel> analiseSocioEconomicaModelOptional = analiseSocioEconomicaService.finByID(id);
+        if (!analiseSocioEconomicaModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("AnaliseSocioEconomica not found.");
+        }
+        var analiseSocioEconomicaModel = new AnaliseSocioEconomicaModel();
+        BeanUtils.copyProperties(analiseSocioEconomicaDto, analiseSocioEconomicaModel);
+        analiseSocioEconomicaModel.setId(analiseSocioEconomicaModelOptional.get().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(analiseSocioEconomicaService.save(analiseSocioEconomicaModel));
+    }
+    
 }
--- src/main/java/com/api/astepi/controllers/PastaAtendimentoController.java
+++ src/main/java/com/api/astepi/controllers/PastaAtendimentoController.java
@@ -8,15 +8,21 @@ import com.api.astepi.models.PastaAtendimentoModel;
 import com.api.astepi.services.EnderecoService;
 import com.api.astepi.services.PastaAtendimentoService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/PastaAtendimentoModel")
+@RequestMapping("/pastaatendimentos")
 public class PastaAtendimentoController {
     final PastaAtendimentoService pastaAtendimentoService;
 
@@ -30,4 +36,41 @@ public class PastaAtendimentoController {
         BeanUtils.copyProperties(pastaAtendimentoDto,pastaAtendimentoModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(pastaAtendimentoService.save(pastaAtendimentoModel));
     }
+
+    @GetMapping
+    public ResponseEntity<Page<PastaAtendimentoModel>> getAllPastaAtendimento(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(pastaAtendimentoService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOnePastaAtendimento(@PathVariable (value = "id") UUID id){
+        Optional<PastaAtendimentoModel> pastaAtendimentoModelOptional = pastaAtendimentoService.finByID(id);
+        if(!pastaAtendimentoModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("PastaAtendimento not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(pastaAtendimentoModelOptional.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletePastaAtendimento(@PathVariable (value = "id")UUID id){
+        Optional<PastaAtendimentoModel> pastaAtendimentoModelOptional = pastaAtendimentoService.finByID(id);
+        if(!pastaAtendimentoModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("PastaAtendimento not found.");
+        }
+        pastaAtendimentoService.delete(pastaAtendimentoModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("PastaAtendimento deleted successfully.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updatePastaAtendimento(@PathVariable(value = "id")UUID id,@RequestBody @Valid PastaAtendimentoDto pastaAtendimentoDto){
+        Optional<PastaAtendimentoModel> pastaAtendimentoModelOptional = pastaAtendimentoService.finByID(id);
+        if (!pastaAtendimentoModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("PastaAtendimento not found.");
+        }
+        var pastaAtendimentoModel = new PastaAtendimentoModel();
+        BeanUtils.copyProperties(pastaAtendimentoDto, pastaAtendimentoModel);
+        pastaAtendimentoModel.setId(pastaAtendimentoModelOptional.get().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(pastaAtendimentoService.save(pastaAtendimentoModel));
+    }
+    
 }

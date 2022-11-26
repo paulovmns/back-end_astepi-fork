0fabd70f7bfbd3d5e865fe23d709f5cc60f75009
paulovmns <94497986+paulovmns@users.noreply.github.com>
2022-11-25 21:11:53 -0300

    Commit by Paulo

---
+++ src/main/java/com/api/astepi/services/AgendamentoService.java
@@ -0,0 +1,39 @@
+package com.api.astepi.services;
+
+import com.api.astepi.models.AgendamentoModel;
+import com.api.astepi.repositories.AgendamentoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class AgendamentoService {
+    final AgendamentoRepository agendamentoRepository;
+
+    public AgendamentoService(AgendamentoRepository agendamentoRepository){
+        this.agendamentoRepository = agendamentoRepository;
+    }
+
+    @Transactional
+    public AgendamentoModel save(AgendamentoModel agendamentoModel) {
+        return agendamentoRepository.save(agendamentoModel);
+    }
+
+    public Page<AgendamentoModel> findAll(Pageable pageable){
+        return agendamentoRepository.findAll(pageable);
+    }
+
+    public Optional<AgendamentoModel> finByID(UUID id) {
+        return agendamentoRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(AgendamentoModel agendamentoModel) {
+        agendamentoRepository.delete(agendamentoModel);
+    }
+
+}

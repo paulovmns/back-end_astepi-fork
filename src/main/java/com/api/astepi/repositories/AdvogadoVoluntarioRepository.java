dc3751fab0adafb4038cba1028d92fedafd4f999
paulovmns <94497986+paulovmns@users.noreply.github.com>
2022-11-25 21:46:06 -0300

    Commit by Paulo - file update/fix

--- src/main/java/com/api/astepi/repositories/AdvogadoVoluntarioRepository.java
+++ src/main/java/com/api/astepi/repositories/AdvogadoVoluntarioRepository.java
@@ -1,6 +1,7 @@
 package com.api.astepi.repositories;
 
 import com.api.astepi.models.AdministradorModel;
+import com.api.astepi.models.AdvogadoVoluntarioModel;
 import com.api.astepi.models.PessoaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,6 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface AdvogadoVoluntarioRepository extends JpaRepository<AdministradorModel, UUID> {
+public interface AdvogadoVoluntarioRepository extends JpaRepository<AdvogadoVoluntarioModel, UUID> {
 
 }

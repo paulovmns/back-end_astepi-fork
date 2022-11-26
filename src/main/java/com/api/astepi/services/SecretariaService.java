fe65b7038a2e4daba7396ac52fd94ec0421f1447
paulovmns <94497986+paulovmns@users.noreply.github.com>
2022-11-25 22:19:16 -0300

    Commit by Paulo - file update/fix

--- src/main/java/com/api/astepi/services/SecretariaService.java
+++ src/main/java/com/api/astepi/services/SecretariaService.java
@@ -1,15 +1,19 @@
 package com.api.astepi.services;
 
+import com.api.astepi.models.AnaliseSocioEconomicaModel;
 import com.api.astepi.models.SecretariaModel;
-import com.api.astepi.models.SecretarioModel;
+import com.api.astepi.repositories.AnaliseSocioEconomicaRepository;
 import com.api.astepi.repositories.SecretariaRepository;
-import com.api.astepi.repositories.SecretarioRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
 
+@Service
 public class SecretariaService {
-    public Object save(SecretariaModel secretariaModel) {
-    }
     final SecretariaRepository secretariaRepository;
 
     public SecretariaService(SecretariaRepository secretariaRepository){
@@ -17,6 +21,21 @@ public class SecretariaService {
     }
 
     @Transactional
-    public SecretariaModel save(SecretariaModel administradorModel) { return secretariaRepository.save(administradorModel); }
+    public SecretariaModel save(SecretariaModel secretariaModel) {
+        return secretariaRepository.save(secretariaModel);
+    }
+
+    public Page<SecretariaModel> findAll(Pageable pageable){
+        return secretariaRepository.findAll(pageable);
+    }
+
+    public Optional<SecretariaModel> finByID(UUID id) {
+        return secretariaRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(SecretariaModel secretariaModel) {
+        secretariaRepository.delete(secretariaModel);
+    }
 
 }

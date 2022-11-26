6a279ebb913d8658dd8a6959feae073ef3edd039
paulovmns <94497986+paulovmns@users.noreply.github.com>
2022-11-25 22:38:11 -0300

    Commit by Paulo

---
+++ src/main/java/com/api/astepi/models/FormularioModel.java
@@ -0,0 +1,45 @@
+package com.api.astepi.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_FORMULARIO")
+public class FormularioModel implements Serializable {
+    private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 255)
+    UsuarioModel usuarioModel;
+
+    @Column(nullable = false, unique = true, length = 255)
+    SecretariaModel secretariaModel;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UsuarioModel getUsuarioModel() {
+        return usuarioModel;
+    }
+
+    public void setUsuarioModel(UsuarioModel usuarioModel) {
+        this.usuarioModel = usuarioModel;
+    }
+
+    public SecretariaModel getSecretariaModel() {
+        return secretariaModel;
+    }
+
+    public void setSecretariaModel(SecretariaModel secretariaModel) {
+        this.secretariaModel = secretariaModel;
+    }
+}

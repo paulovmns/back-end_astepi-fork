f923a60da75c703cd16620d381a2b7c13765297a
paulovmns <94497986+paulovmns@users.noreply.github.com>
2022-11-25 22:18:52 -0300

    Commit by Paulo - file update/fix

--- src/main/java/com/api/astepi/dtos/SecretariaDto.java
+++ src/main/java/com/api/astepi/dtos/SecretariaDto.java
@@ -1,4 +1,20 @@
 package com.api.astepi.dtos;
 
+import com.api.astepi.models.AdvogadoVoluntarioModel;
+import com.api.astepi.models.UsuarioModel;
+
+import javax.validation.constraints.NotBlank;
+
 public class SecretariaDto {
+
+    @NotBlank
+    FormularioDto formularioDto;
+
+    public FormularioDto getFormularioDto() {
+        return formularioDto;
+    }
+
+    public void setFormularioDto(FormularioDto formularioDto) {
+        this.formularioDto = formularioDto;
+    }
 }

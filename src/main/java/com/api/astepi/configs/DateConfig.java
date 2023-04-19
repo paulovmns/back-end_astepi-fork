ffda9aaac636ff42c2a11a2e8fa60a3f79d2c748
paulovmns <94497986+paulovmns@users.noreply.github.com>
2023-04-19 01:41:54 -0300

    Update DateConfig.java

--- src/main/java/com/api/astepi/configs/DateConfig.java
+++ src/main/java/com/api/astepi/configs/DateConfig.java
@@ -7,11 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import com.zaxxer.hikari.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
-import javax.sql.DataSource;
-
 import java.time.format.DateTimeFormatter;
 
 @Configuration
@@ -28,20 +23,3 @@ public class DateConfig {
                 .registerModule(module);
     }
 }
-    
- @Configuration
-    public class DatabaseConfig {
-
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
-
-  @Bean
-  public DataSource dataSource() {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-  }
-    
- }   
-
-

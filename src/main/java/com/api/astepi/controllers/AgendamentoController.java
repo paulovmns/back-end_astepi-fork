92c69d4026aed11ed784075179de020f50b15092
paulovmns <94497986+paulovmns@users.noreply.github.com>
2023-05-01 22:11:50 -0300

    controller agendamento bug fix CROSS

--- src/main/java/com/api/astepi/controllers/AgendamentoController.java
+++ src/main/java/com/api/astepi/controllers/AgendamentoController.java
@@ -24,6 +24,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/agendamentos")
 public class AgendamentoController {
 
